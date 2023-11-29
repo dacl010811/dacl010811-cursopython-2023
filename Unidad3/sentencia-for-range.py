fe117e2f7672c89a15b4719fe5cf15fc22f05ba2
dacl010811 <dacl010811@gmail.com>
2023-11-28 21:43:47 -0500

    Range

---
+++ Unidad3/sentencia-for-range.py
@@ -0,0 +1,16 @@
+
+
+
+# funcion range () ---> Genera numeros
+# funcion range (valor), range (valor1, valor2), range (valor1, valor2,valor3)
+
+for numero in range(10): # Generar numero desde el 0  hasta  limite (10) -1:   ---> (0-9)
+    print(numero)
+    
+for valor in range(1, 21): # Generar numero desde el 0  hasta  limite (21) - 1:   ---> (1-20)
+    print(f"Dato : [{valor}]")
+
+for valor_1 in range(0,11,2): # Generar numero desde el 0  hasta  limite (21) - 1:   ---> (1-20) # 0 1 2 3 4 5 6 7 8 9 10                            
+    print(f"Rango : [{valor_1}]")
+
+
