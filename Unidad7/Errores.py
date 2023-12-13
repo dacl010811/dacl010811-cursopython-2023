901615d203335c33eaa3cd7e9e8bb71b8c5e273d
dacl010811 <dacl010811@gmail.com>
2023-12-12 20:03:21 -0500

    Excepciones Python Raise

--- Unidad7/Errores.py
+++ Unidad7/Errores.py
@@ -37,5 +37,56 @@ def  division():
 
     print("Finalizo el programa!!")
 
-division()
+def  divisionFinally():
     
+    valor_1 = input("Ingrese un valor 1: \n")
+    valor_2 = input("Ingrese un valor 2: \n")
+    
+    try:
+        resultado = valor_1 / valor_2
+    except TypeError as error:
+        print(f"Error de Tipo : {type(error)}")    
+    except Exception as error:
+        print(f"Excepcion General : {type(error)}")
+    else:
+        print("Funciono correctamente y entro al else")
+    finally:
+        print("Siempre pasaré por aqui!!!")
+        resultado = 0
+        try:
+            valor_1 = float(valor_1)
+            valor_2 = float(valor_2)
+            resultado = valor_1 / valor_2
+        except ValueError as ve:
+            print(f"Error interno finally: {type(ve)}")
+        except UnboundLocalError as ve:
+            print(f"Error UnboundLocalError finally: {type(ve)}")
+            resultado = 0
+        except  Exception as error:
+            print(f"Error General interno finally: {type(error)}")
+        
+        print(f"El resultado es : {resultado}")
+    
+#divisionFinally()
+
+# La instrucción raise
+
+def presentarMensaje(mensaje=None):
+    try:
+        if mensaje is None:
+            raise  ValueError   
+    except ValueError as ve:
+        print(f"LLamado por raise: {type(ve).__name__}")
+    except ZeroDivisionError as ve:
+        print(f"LLamado por raise: {type(ve).__name__}")
+    except TypeError as ve:
+        print(f"LLamado por raise: {type(ve).__name__}")
+    except:
+        print(f"Error General")
+    else:
+        print(f"El flujo fue normal, sin excepciones!!!")
+    finally:
+        print(f" Soy finally : Darwin : {mensaje}")
+
+presentarMensaje()
+presentarMensaje("Bienvenido")
