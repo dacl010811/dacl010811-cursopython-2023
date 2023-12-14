c072f25bbefd16b836c5a6b9d12e6841223cc011
dacl010811 <dacl010811@gmail.com>
2023-12-13 21:38:29 -0500

    OK Unidad9

---
+++ Unidad9/poo.py
@@ -0,0 +1,40 @@
+
+
+class Persona():
+    """Representa un objeto de la vida: persona"""
+    
+    nombre = ""
+    apellido = ""
+    edad = ""
+    celular = ""
+    
+    def __init__(self,nombre=None,apellido=None,edad=None):
+        pass
+    
+    def dormir():
+        pass
+    
+    def trabajar():
+        pass
+    
+    def correr():
+        pass
+    
+    def respirar():
+        pass
+    
+    
+    
+    
+    
+
+class Automovil():
+    pass
+
+class Amimales():
+    pass
+
+class Transporte():
+    pass
+
+
