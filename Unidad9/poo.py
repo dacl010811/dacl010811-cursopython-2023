ba2e238b53bd4bc2b4e82f67bab7462c0d609e20
dacl010811 <dacl010811@gmail.com>
2023-12-14 20:08:58 -0500

    OK poo

--- Unidad9/poo.py
+++ Unidad9/poo.py
@@ -3,38 +3,53 @@
 class Persona():
     """Representa un objeto de la vida: persona"""
     
-    nombre = ""
-    apellido = ""
-    edad = ""
-    celular = ""
+    #Atributos de Clase
+    contador_personas = 0
+    etnia_personas = "Afro"
     
+    #Constructor
     def __init__(self,nombre=None,apellido=None,edad=None):
-        pass
+                
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
     
-    def dormir():
-        pass
+    @classmethod
+    def dormir(cls):
+        print("Estoy durmiendo!!  - Metodo de Clase ")
     
-    def trabajar():
-        pass
+    def trabajar(self):
+        print("Estoy trabajando!!")
     
-    def correr():
-        pass
+    @classmethod
+    def correr(cls):
+        print("Estoy corriendo!! - Metodo de Clase")
     
-    def respirar():
-        pass
-    
-    
-    
-    
-    
+    def respirar(self):
+        print("Estoy vivo!!")
 
-class Automovil():
-    pass
 
-class Amimales():
-    pass
+#Acceder a los atributos y metodos de clase  
+#Atributos
+Persona.contador_personas = 10     #  set()
+Persona.etnia_personas = "Meztiso"     #  set()
+    
+# Crear instancias de una clase    
+persona_1  = Persona("Darwin","Calle", 40)    
+#Acceder a los atributos de instancia
+print(persona_1.nombre)
+print(persona_1.apellido)
+print(persona_1.edad)
 
-class Transporte():
-    pass
 
+# Acceder a los metodos de clase
+Persona.dormir()
+Persona.correr()
+
+
+#Acceder a los metodos de instancia
+persona_2 = Persona("Maria","Calle",4)
+
+persona_2.respirar()
+persona_2.trabajar()
 
