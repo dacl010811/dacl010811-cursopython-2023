854e6c5e4d62d027e5a10ab2542b3754e8251e92
dacl010811 <dacl010811@gmail.com>
2023-12-15 20:31:42 -0500

    Zoologico

--- Unidad9/modulo.py
+++ Unidad9/modulo.py
@@ -1,5 +1,6 @@
 
 
+
 class Persona():
     contador_personas = 0
     
@@ -39,12 +40,120 @@ class Cliente(Persona):
         print("Voy a comprar")
         
 
-# Crear instancias de cliente
-cliente_1  = Cliente("19055454545","Nicolas","Quito","0998877544","Quito y Amazonas","Amazonas","022548754")
-print(cliente_1)
+# Diseñar algo relacionado a un Zoologico
 
-cliente_1.dormir()
-cliente_1.trabajar()
-cliente_1.comprar()
+class Animal():
+    
+    def __init__(self, nombre,especie,edad,region) -> None:
+        self.nombre = nombre
+        self.especie = especie
+        self.edad = edad
+        self.region = region
         
+    def comer(self):
+        print("Estoy comiendo: Clase Aminal")
         
+    def dormir(self):
+        print("Estoy durmiendo: Clase Aminal")
+
+class Mamifero(Animal):
+    
+    def __init__(self, nombre,especie,edad,region,pelaje,color):
+        super().__init__( nombre,especie,edad,region)
+        self.pelaje = pelaje
+        self.color = color
+    
+    def amamantar(self):
+        print("Estoy amamantando a las crias")
+        
+    def comer(self):
+        print("Estoy comiendo, pero soy un mamifero")
+    
+    def dormir(self):
+         print("Estoy durmiendo como un mamifero")
+
+
+class Ave(Animal):
+    
+    def __init__(self, nombre,especie,edad,region,color):
+        super().__init__( nombre,especie,edad,region)        
+        self.color = color
+        
+    def volar(self):
+        print ("Estoy volando, soy una Ave")
+
+
+class Reptil(Animal):
+    
+    def __init__(self, nombre,especie,edad,region,tipo_piel):
+        super().__init__( nombre,especie,edad,region)        
+        self.tipo_piel = tipo_piel
+    
+    def arrastrarse(self):
+        print("Estoy arrastrandome")
+
+
+class Anfibio(Animal):
+    
+    def __init__(self, nombre,especie,edad,region,tipo_anfibio):
+        super().__init__( nombre,especie,edad,region)        
+        self.tipo_anfibio = tipo_anfibio
+
+
+class AreaZoologico():
+    
+    def __init__(self,nombre,capacidad):
+        self.nombre = nombre
+        self.capacidad = capacidad        
+        self.aminales = []
+        
+    def agregar_animal(self, animal):
+        if len(self.aminales) < self.capacidad:
+            self.aminales.append(animal)
+            print(f" El animal: {animal} ha sido agregado al area : {self.nombre}")
+        else:
+            print(f" No hay espacio disponible para agregarlo")
+    
+    
+class Zoologico():
+    
+    def __init__(self,nombre,direccion):
+        self.nombre = nombre
+        self.direccion = direccion
+        self.areas = []
+        
+    def agregar_area(self, area):
+        self.areas.append(area)
+        print(f"El creado el area : {area.nombre}, en el zoologico!!")
+        
+
+# Crear objetos o instancias de una clase
+
+leon = Mamifero("Simba","Leon",5,"Africa","Suave","Cafe")
+perro = Mamifero("Dog","Perro",5,"Suiza","Suave","Negro")
+
+aguila = Ave("Aguila Real","Aguila",3,"Sierra","Cafe")
+condor = Ave("Condor Sierra","Condor",10,"Sierra","Negro")
+
+serpiente = Reptil("Boa","Serpiente",10,"Amazonia")
+
+#Areas del Zoologico
+zona_maniferos = AreaZoologico("Aminales Mamiferos",5)
+zona_maniferos.agregar_animal(leon)
+zona_maniferos.agregar_animal(perro)
+
+zona_aves = AreaZoologico("Aminales Aves",10)
+zona_aves.agregar_animal(aguila)
+zona_aves.agregar_animal(condor)
+
+zona_reptiles = AreaZoologico("Aminales Reptiles",8)
+zona_reptiles.agregar_animal(serpiente)
+
+
+zoologico_aventura = Zoologico("Aventura Animal","Quito Norte")
+zoologico_aventura.agregar_area(zona_maniferos)
+zoologico_aventura.agregar_area(zona_aves)
+zoologico_aventura.agregar_area(zona_reptiles)
+
+
+
